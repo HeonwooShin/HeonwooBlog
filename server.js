1b58ebb2204044fbdaeb11793e10ddac3e03f708
HeonwooShin <scienceheonu@gmail.com>
2022-10-02 11:09:56 +0900

    adapting tailwind

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+console.log(__dirname)
+
+app.use(express.static(__dirname + '/dist'));
+
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../pages/index.html'));
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
 app.listen(PORT, function() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/src/pages/*.html"],
+  content: ["src/pages/*.html",
+            "src/js/*.js",
+            "./index.html"],
   theme: {
     extend: {},
   },
